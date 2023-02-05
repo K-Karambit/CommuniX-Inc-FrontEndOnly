2ad3e3844996840aa256648fc0cda9c33e4972ca
Kenneth Serrano <kennethserrano61001@gmail.com>
2023-02-05 10:57:20 +0800

    February 5, 2023

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function showAndHide() {
+  document.getElementById("form1").classList.add("d-none");
+  document.getElementById("form2").classList.remove("d-none");
+  document.getElementById("form1").classList.remove("d-block");
+  document.getElementById("form2").classList.add("d-block");
+}
